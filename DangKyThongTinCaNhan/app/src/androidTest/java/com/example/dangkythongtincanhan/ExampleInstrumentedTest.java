c01fb050d83e27264041421f3075151bf1573b27
CongNho <hocongnhodtu@gmail.com>
2019-09-12 09:00:13 +0700

    + Add project DangKyThongTinSinhVien

--- DangKyThongTinCaNhan/app/src/androidTest/java/com/example/dangkythongtincanhan/ExampleInstrumentedTest.java
+++ DangKyThongTinCaNhan/app/src/androidTest/java/com/example/dangkythongtincanhan/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.example;
+package com.example.dangkythongtincanhan;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.example", appContext.getPackageName());
+        assertEquals("com.example.dangkythongtincanhan", appContext.getPackageName());
     }
 }
